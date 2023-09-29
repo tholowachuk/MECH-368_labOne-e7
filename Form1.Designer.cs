ff5dbc02d5ab97a6a754f2d0af6a4c429859a7b6
tholowachuk <tim.holowachuk@outlook.com>
2023-09-28 22:30:48 -0700

    done to Exercise 8

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,11 +36,19 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.buttonDataProcess = new System.Windows.Forms.Button();
-            this.textBoxCurrentState = new System.Windows.Forms.TextBox();
+            this.textBoxCurrentMovement = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.textBoxDataDisplay = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonUpdateSerial = new System.Windows.Forms.Button();
+            this.comboBoxCOMPorts = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxGestures = new System.Windows.Forms.TextBox();
+            this.textBoxFilename = new System.Windows.Forms.TextBox();
+            this.buttonFilename = new System.Windows.Forms.Button();
+            this.checkBoxSave = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // textBoxAX
@@ -103,12 +111,12 @@
             this.buttonDataProcess.UseVisualStyleBackColor = true;
             this.buttonDataProcess.Click += new System.EventHandler(this.buttonDataProcess_Click);
             // 
-            // textBoxCurrentState
+            // textBoxCurrentMovement
             // 
-            this.textBoxCurrentState.Location = new System.Drawing.Point(389, 67);
-            this.textBoxCurrentState.Name = "textBoxCurrentState";
-            this.textBoxCurrentState.Size = new System.Drawing.Size(100, 20);
-            this.textBoxCurrentState.TabIndex = 8;
+            this.textBoxCurrentMovement.Location = new System.Drawing.Point(389, 67);
+            this.textBoxCurrentMovement.Name = "textBoxCurrentMovement";
+            this.textBoxCurrentMovement.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCurrentMovement.TabIndex = 8;
             // 
             // label3
             // 
@@ -121,16 +129,16 @@
             // 
             // textBoxDataDisplay
             // 
-            this.textBoxDataDisplay.Location = new System.Drawing.Point(12, 93);
+            this.textBoxDataDisplay.Location = new System.Drawing.Point(12, 119);
             this.textBoxDataDisplay.Multiline = true;
             this.textBoxDataDisplay.Name = "textBoxDataDisplay";
-            this.textBoxDataDisplay.Size = new System.Drawing.Size(477, 345);
+            this.textBoxDataDisplay.Size = new System.Drawing.Size(477, 319);
             this.textBoxDataDisplay.TabIndex = 10;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 70);
+            this.label5.Location = new System.Drawing.Point(9, 103);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(65, 13);
             this.label5.TabIndex = 11;
@@ -139,16 +147,96 @@
             // serialPort1
             // 
             this.serialPort1.PortName = "COM3";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 70);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "COM Port";
+            // 
+            // buttonUpdateSerial
+            // 
+            this.buttonUpdateSerial.Location = new System.Drawing.Point(200, 65);
+            this.buttonUpdateSerial.Name = "buttonUpdateSerial";
+            this.buttonUpdateSerial.Size = new System.Drawing.Size(108, 23);
+            this.buttonUpdateSerial.TabIndex = 14;
+            this.buttonUpdateSerial.UseVisualStyleBackColor = true;
+            this.buttonUpdateSerial.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // comboBoxCOMPorts
+            // 
+            this.comboBoxCOMPorts.FormattingEnabled = true;
+            this.comboBoxCOMPorts.Location = new System.Drawing.Point(71, 66);
+            this.comboBoxCOMPorts.Name = "comboBoxCOMPorts";
+            this.comboBoxCOMPorts.Size = new System.Drawing.Size(123, 21);
+            this.comboBoxCOMPorts.TabIndex = 15;
+            this.comboBoxCOMPorts.SelectedIndexChanged += new System.EventHandler(this.comboBoxCOMPorts_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 455);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(130, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Last Gesture Recognized:";
+            // 
+            // textBoxGestures
+            // 
+            this.textBoxGestures.Location = new System.Drawing.Point(145, 452);
+            this.textBoxGestures.Name = "textBoxGestures";
+            this.textBoxGestures.Size = new System.Drawing.Size(344, 20);
+            this.textBoxGestures.TabIndex = 17;
+            // 
+            // textBoxFilename
+            // 
+            this.textBoxFilename.Location = new System.Drawing.Point(146, 478);
+            this.textBoxFilename.Name = "textBoxFilename";
+            this.textBoxFilename.Size = new System.Drawing.Size(257, 20);
+            this.textBoxFilename.TabIndex = 18;
+            // 
+            // buttonFilename
+            // 
+            this.buttonFilename.Location = new System.Drawing.Point(12, 476);
+            this.buttonFilename.Name = "buttonFilename";
+            this.buttonFilename.Size = new System.Drawing.Size(127, 23);
+            this.buttonFilename.TabIndex = 19;
+            this.buttonFilename.Text = "Select Filename";
+            this.buttonFilename.UseVisualStyleBackColor = true;
+            this.buttonFilename.Click += new System.EventHandler(this.buttonFilename_Click);
+            // 
+            // checkBoxSave
+            // 
+            this.checkBoxSave.AutoSize = true;
+            this.checkBoxSave.Enabled = false;
+            this.checkBoxSave.Location = new System.Drawing.Point(407, 480);
+            this.checkBoxSave.Name = "checkBoxSave";
+            this.checkBoxSave.Size = new System.Drawing.Size(82, 17);
+            this.checkBoxSave.TabIndex = 20;
+            this.checkBoxSave.Text = "Save to File";
+            this.checkBoxSave.UseVisualStyleBackColor = true;
             // 
             // StateMachine
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(502, 450);
+            this.ClientSize = new System.Drawing.Size(502, 511);
+            this.Controls.Add(this.checkBoxSave);
+            this.Controls.Add(this.buttonFilename);
+            this.Controls.Add(this.textBoxFilename);
+            this.Controls.Add(this.textBoxGestures);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBoxCOMPorts);
+            this.Controls.Add(this.buttonUpdateSerial);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBoxDataDisplay);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBoxCurrentState);
+            this.Controls.Add(this.textBoxCurrentMovement);
             this.Controls.Add(this.buttonDataProcess);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
@@ -156,7 +244,7 @@
             this.Controls.Add(this.textBoxAZ);
             this.Controls.Add(this.textBoxAY);
             this.Controls.Add(this.textBoxAX);
-            this.MinimumSize = new System.Drawing.Size(518, 489);
+            this.MinimumSize = new System.Drawing.Size(518, 525);
             this.Name = "StateMachine";
             this.Text = "State Machine";
             this.Load += new System.EventHandler(this.StateMachine_Load);
@@ -174,11 +262,19 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button buttonDataProcess;
-        private System.Windows.Forms.TextBox textBoxCurrentState;
+        private System.Windows.Forms.TextBox textBoxCurrentMovement;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBoxDataDisplay;
         private System.Windows.Forms.Label label5;
         private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonUpdateSerial;
+        private System.Windows.Forms.ComboBox comboBoxCOMPorts;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxGestures;
+        private System.Windows.Forms.TextBox textBoxFilename;
+        private System.Windows.Forms.Button buttonFilename;
+        private System.Windows.Forms.CheckBox checkBoxSave;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -21,75 +21,411 @@ namespace MECH_368_labOne_e7
         int currentAZValue = 0;
         int currentByteIndex = 0;
         int bytesToReadDisplay = 0;
+        int currentState = 0;
+        bool isXPlusDetected = false;
+        bool isYPlusDetected = false;
+        bool isZPlusDetected = false;
+        string lastDetectedSequence = "";
+        StringBuilder orientationStringBuilder = new StringBuilder();
+        DateTime sequenceStartTime;
         ConcurrentQueue<Int32> dataQueue = new ConcurrentQueue<Int32>();
         ConcurrentQueue<Int32> aXQueue = new ConcurrentQueue<Int32>();
         ConcurrentQueue<Int32> aYQueue = new ConcurrentQueue<Int32>();
         ConcurrentQueue<Int32> aZQueue = new ConcurrentQueue<Int32>();
+
+        private StreamWriter csvWriter;
 
         public StateMachine()
         {
             InitializeComponent();
         }
 
-
-
         private void StateMachine_Load(object sender, EventArgs e)
         {
-            System.Windows.Forms.Timer queuetimer = new System.Windows.Forms.Timer();
-            queuetimer.Interval = 100;
-            queuetimer.Tick += new EventHandler(queueTimer_Tick);
-            queuetimer.Start();
+            //clear the dropdown box, add currently used COM ports to it for selection
+            comboBoxCOMPorts.Items.Clear();
+            comboBoxCOMPorts.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
+            if (comboBoxCOMPorts.Items.Count > 0)
+                comboBoxCOMPorts.SelectedIndex = 0;
+            else
+                comboBoxCOMPorts.Text = "No COM ports!";
+
+            //create a timer for events to occur within
+            System.Windows.Forms.Timer queueTimer = new System.Windows.Forms.Timer();
+            queueTimer.Interval = 100;
+            queueTimer.Tick += new EventHandler(queueTimer_Tick);
+            queueTimer.Start();
         }
 
-        //initialize error flag for timer
-        private bool errorShown = false;
-        private string selectedCOMPort = null;
-
-        private void queueTimer_Tick(object sender, EventArgs e)
+        private string DetermineOrientation(int ax, int ay, int az)
         {
-            try
-            {            
-                string[] COMPorts = System.IO.Ports.SerialPort.GetPortNames();
+            // Define thresholds for each axis
+            int XThreshold = 129;
+            int YThreshold = 129;
+            int ZThreshold = 151;
+            int trueX = ax - XThreshold;
+            int trueY = ay - YThreshold;
+            int trueZ = az - ZThreshold;
+            int thresholdDistance = 8;
 
-                //check if the selected COM port is in the list of available ports
-                if (!string.IsNullOrEmpty(selectedCOMPort) && !COMPorts.Contains(selectedCOMPort))
+            // Calculate the distances from each threshold along each axis
+            int distanceX = Math.Abs(trueX) - thresholdDistance;
+            int distanceY = Math.Abs(trueY) - thresholdDistance;
+            int distanceZ = Math.Abs(trueZ) - thresholdDistance;
+
+            // Check if any axis has moved more than the threshold distance
+            bool isMovingX = distanceX > thresholdDistance;
+            bool isMovingY = distanceY > thresholdDistance;
+            bool isMovingZ = distanceZ > thresholdDistance;
+
+            // Determine the orientation based on the maximum distance
+            if (isMovingX || isMovingY || isMovingZ)
+            {
+                if (distanceX >= distanceY && distanceX >= distanceZ)
                 {
-                    if (serialPort1.IsOpen)
+                    if (trueX > thresholdDistance)
                     {
-                        serialPort1.Close();
+                        currentState = 1;
+                        return "Moving X+";
                     }
-                    selectedCOMPort = null;
-                    errorShown = false;
-                }
-                
-                //check if there are any available COM ports
-                if (COMPorts.Length >= 1)
-                {
-                    //check if port is open or not
-                    if (!serialPort1.IsOpen)
+                    else
                     {
-                        //using first available COM port, open and request acceleration data
-                        serialPort1.PortName = COMPorts[0];
-                        serialPort1.PortName = selectedCOMPort;
-                        serialPort1.Open();
-                        serialPort1.Write("A");
-                        MessageBox.Show($"Serial port {serialPort1.PortName} is open!", "Port Connected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        currentState = 2;
+                        return "Moving X-";
+                    }
+                }
+                else if (distanceY >= distanceX && distanceY >= distanceZ)
+                {
+                    if (trueY > thresholdDistance)
+                    {
+                        currentState = 3;
+                        return "Moving Y+";
+                    }
+                    else
+                    {
+                        currentState = 4;
+                        return "Moving Y-";
+                    }
+                }
+                else
+                {
+                    if (trueZ > thresholdDistance)
+                    {
+                        currentState = 5;
+                        return "Moving Z+";
+                    }
+                    else
+                    {
+                        currentState = 6;
+                        return "Moving Z-";
                     }
                 }
             }
-            catch (Exception ex)
+            else
             {
-                if (!errorShown)
+                currentState = 0;
+                return "Resting";
+            }
+        }
+
+        private void DetectSequences(string orientationString)
+        {
+            // Define the three sequences to detect
+            string sequence1 = "Moving X+";
+            string sequence2 = "Moving Z+Moving X+";
+            string sequence3 = "Moving X+Moving Y+Moving Z+";
+
+            // Check if any of the sequences are present in the orientation string
+            if (orientationString.Contains(sequence1))
+            {
+                lastDetectedSequence = "Simple punch";
+            }
+            else if (orientationString.Contains(sequence2))
+            {
+                lastDetectedSequence = "High punch";
+            }
+            else if (orientationString.Contains(sequence3))
+            {
+                lastDetectedSequence = "Right hook";
+            }
+            else
+            {
+                lastDetectedSequence = "";
+            }
+
+            // Display the last detected sequence in the textBoxGestures using Invoke
+            this.Invoke((MethodInvoker)delegate
+            {
+                textBoxGestures.Text = lastDetectedSequence;
+            });
+        }
+
+        private async void queueTimer_Tick(object sender, EventArgs e)
+        {
+            //check if board is ready to be connected
+            if (!serialPort1.IsOpen)
+            {
+                //update button text with relevant label
+                buttonUpdateSerial.Text = "Connect Serial";
+
+                //2023-09-17 - clear text that might have been missed, bug patch
+                textBoxDataDisplay.Text = "";
+            }
+            else
+            {
+                //update button text with relevant label
+                buttonUpdateSerial.Text = "Disconnect Serial";
+            }
+            
+            //dequeue items and insert them into an endless textbox
+            while (dataQueue.TryDequeue(out int dequeuedByte))
+            {
+                //check if next item is 255
+                if (dequeuedByte == 255)
                 {
-                    MessageBox.Show("Error opening the serial port: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    errorShown = true;
+                    currentByteIndex = 0;
                 }
+                else
+                {
+                    if (currentByteIndex == 0)
+                    {
+                        currentAXValue = dequeuedByte;
+                    }
+                    else if (currentByteIndex == 1)
+                    {
+                        currentAYValue = dequeuedByte;
+                    }
+                    else if (currentByteIndex == 2)
+                    {
+                        currentAZValue = dequeuedByte;
+                    }
+
+                    currentByteIndex++;
+
+                    aXQueue.Enqueue(currentAXValue);
+                    aYQueue.Enqueue(currentAYValue);
+                    aZQueue.Enqueue(currentAZValue);
+
+                    textBoxAX.Text = currentAXValue.ToString();
+                    textBoxAY.Text = currentAYValue.ToString();
+                    textBoxAZ.Text = currentAZValue.ToString();
+                }
+
+            }
+
+            // Update state machine textbox with current state of the accelerometer
+            if (serialPort1.IsOpen)
+            {
+                string currentMovement = DetermineOrientation(currentAXValue, currentAYValue, currentAZValue);
+                textBoxCurrentMovement.Text = currentMovement;
+
+                // Append the current orientation to the string
+                orientationStringBuilder.Append(currentMovement);
+
+                // Limit the length of the orientation string to prevent excessive growth
+                if (orientationStringBuilder.Length > 1000)
+                {
+                    orientationStringBuilder.Remove(0, orientationStringBuilder.Length - 1000);
+                }
+
+                // Detect X+, Y+, and Z+ movements and set corresponding flags
+                if (currentMovement == "Moving X+")
+                {
+                    isXPlusDetected = true;
+                }
+                else if (currentMovement == "Moving Y+")
+                {
+                    isYPlusDetected = true;
+                }
+                else if (currentMovement == "Moving Z+")
+                {
+                    isZPlusDetected = true;
+                }
+
+                // Check for sequence presence after 250ms
+                await Task.Delay(250);
+
+                if (isXPlusDetected && isYPlusDetected && isZPlusDetected)
+                {
+                    // Detected "Moving X+Moving Y+Moving Z+" sequence
+                    lastDetectedSequence = "Right hook";
+
+                    // Clear flags and orientation string
+                    isXPlusDetected = false;
+                    isYPlusDetected = false;
+                    isZPlusDetected = false;
+                    orientationStringBuilder.Clear();
+
+                    // Display the last detected sequence in the textBoxGestures using Invoke
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        textBoxGestures.Text = lastDetectedSequence;
+                    });
+
+                    // Clear the textbox after 1 second
+                    await Task.Delay(1000);
+
+                    // Clear the textbox using Invoke
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        textBoxGestures.Text = "";
+                    });
+                }
+                else if (isZPlusDetected && isXPlusDetected)
+                {
+                    // Detected "Moving X+Moving Y+Moving Z+" sequence
+                    lastDetectedSequence = "High punch";
+
+                    // Clear flags and orientation string
+                    isXPlusDetected = false;
+                    isYPlusDetected = false;
+                    isZPlusDetected = false;
+                    orientationStringBuilder.Clear();
+
+                    // Display the last detected sequence in the textBoxGestures using Invoke
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        textBoxGestures.Text = lastDetectedSequence;
+                    });
+
+                    // Clear the textbox after 1 second
+                    await Task.Delay(1000);
+
+                    // Clear the textbox using Invoke
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        textBoxGestures.Text = "";
+                    });
+                }
+                else if (isXPlusDetected)
+                {
+                    // Detected "Moving X+Moving Y+Moving Z+" sequence
+                    lastDetectedSequence = "Simple punch";
+
+                    // Clear flags and orientation string
+                    isXPlusDetected = false;
+                    isYPlusDetected = false;
+                    isZPlusDetected = false;
+                    orientationStringBuilder.Clear();
+
+                    // Display the last detected sequence in the textBoxGestures using Invoke
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        textBoxGestures.Text = lastDetectedSequence;
+                    });
+
+                    // Clear the textbox after 1 second
+                    await Task.Delay(1000);
+
+                    // Clear the textbox using Invoke
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        textBoxGestures.Text = "";
+                    });
+                }
+                //else
+                //{
+                //    await Task.Delay(1000);
+                    
+                //    // Clear flags and orientation string
+                //    isXPlusDetected = false;
+                //    isYPlusDetected = false;
+                //    isZPlusDetected = false;
+                //    orientationStringBuilder.Clear();
+                //}
+            }
+            else
+            {
+                textBoxCurrentMovement.Text = null;
+            }
+
+            //check if the save checkbox is selected, write data if so
+            if (checkBoxSave.Checked)
+            {
+                if (csvWriter == null)
+                {
+                    try
+                    {
+                        csvWriter = new StreamWriter(textBoxFilename.Text, false);
+                        csvWriter.WriteLine("Ax, Ay, Az, Timestamp");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error writing data: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+                string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+                csvWriter.WriteLine($"{currentAXValue}, {currentAYValue}, {currentAZValue}, {timestamp}, ");
             }
         }
 
         private void buttonDataProcess_Click(object sender, EventArgs e)
         {
+            textBoxDataDisplay.AppendText("(" + currentAXValue + ", " + currentAYValue + ", " + currentAZValue + ", " + currentState + "), ");
+        }
 
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            //implementing some error handling here using exception messaging
+            try
+            {
+                //if the port is closed, open it and request acceleration data; if the port is open, close and clear text fields
+                if (!serialPort1.IsOpen)
+                {
+                    serialPort1.Open();
+                    serialPort1.Write("A");
+                }
+                else
+                {
+                    serialPort1.Close();
+                    textBoxDataDisplay.Text = "";
+                    textBoxAX.Text = "";
+                    textBoxAY.Text = "";
+                    textBoxAZ.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error opening the serial port: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int bytesToRead;
+            int newByte;
+
+            bytesToRead = serialPort1.BytesToRead;
+            bytesToReadDisplay = bytesToRead;
+
+            while (bytesToRead > 0)
+            {
+                newByte = serialPort1.ReadByte();
+                dataQueue.Enqueue(newByte);
+                bytesToRead = serialPort1.BytesToRead;
+            }
+        }
+
+        private void comboBoxCOMPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comboBoxCOMPorts.Text;
+        }
+
+        private void buttonFilename_Click(object sender, EventArgs e)
+        {
+            //initiate a save box
+            SaveFileDialog csvSave = new SaveFileDialog();
+            csvSave.Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*";
+
+            if (csvSave.ShowDialog() == DialogResult.OK)
+            {
+                //display selected save path & start data recording
+                string filePath = csvSave.FileName;
+                textBoxFilename.Text = filePath;
+                checkBoxSave.Enabled = true;
+                checkBoxSave.Checked = true;
+            }
         }
     }
 }
